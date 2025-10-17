3b04de94e0860a1fe43c773cd38f3b6f3ca731f8
Isaac.Lera <isaac.lera@gmail.com>
2025-10-17 10:34:48 +0200

    Fix compatibility with readthedocs platform

---
+++ docs/conf.py
@@ -0,0 +1,54 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'TTAD-master'
+copyright = '2024, TTAD Team'
+author = 'TTAD Team'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'nbsphinx',
+    'sphinx.ext.mathjax',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- Extension configuration -------------------------------------------------
+
+# nbsphinx configuration
+nbsphinx_execute = 'never'  # Don't execute notebooks during build
+nbsphinx_allow_errors = True
+
+# Napoleon settings for Google/NumPy style docstrings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
---
+++ docs/index.rst
@@ -0,0 +1,76 @@
+.. TxADM documentation master file, created by
+   sphinx-quickstart on Tue Jun 21 07:10:22 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to TTAD-master's documentation!
+=================================
+
+
+Part 1
+=================================
+
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Introducción a Python
+
+   ../notebooks/01_Python/01_Introduction
+   ../notebooks/01_Python/02_BasicConcepts
+   ../notebooks/01_Python/03_ControlStatements
+   ../notebooks/01_Python/04_OtherTypes
+   
+.. toctree::
+   :maxdepth: 2
+   :caption: Librería Numpy
+   
+   ../notebooks/02_Numpy/01_Introduction
+
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Pandas
+
+   ../notebooks/03_Pandas/01_Introduction
+   ../notebooks/03_Pandas/02_GroupingData
+   ../notebooks/03_Pandas/03_OtherActions
+   ../notebooks/03_Pandas/04_Visualization
+
+.. toctree::
+   :maxdepth: 2
+   :caption: ETL Process
+
+   ../notebooks/04_ETLprocess/01_Introduction
+   ../notebooks/04_ETLprocess/02_Automatization
+   ../notebooks/04_ETLprocess/03_Orchestration
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Machine Learning techniques
+
+   ../notebooks/05_MachineLearning/01_Introduction
+   ../notebooks/05_MachineLearning/02_PredictiveModelling
+   ../notebooks/05_MachineLearning/03_Activity_LinearRegression
+   ../notebooks/05_MachineLearning/04_Regression
+   ../notebooks/05_MachineLearning/04b_Ejercicio1
+   ../notebooks/05_MachineLearning/04b_Ejercicio2
+   ../notebooks/05_MachineLearning/05_Classification
+   ../notebooks/05_MachineLearning/06_Clustering
+
+   
+
+
+
+.. Indices and tables
+.. ==================
+
+.. * :ref:`genindex`
+.. * :ref:`modindex`
+.. * :ref:`search`
+
+
+.. note::
+   
+   This project is under active development.
+
+
